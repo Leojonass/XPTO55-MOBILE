8ca68c5353b56f3e8ae66c1511d1ca38429a7898
Leojonass <leo.serafim@bandtec.com.br>
2023-04-18 21:13:35 -0300

    primeiro commit

---
+++ CPAweb/app/src/main/java/com/example/cpaweb/pro_register.kt
@@ -0,0 +1,11 @@
+package com.example.cpaweb
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class pro_register : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pro_register)
+    }
+}
